488320acb4172c1e38d775e3561ae755e9a9cfcc
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-12-13 18:24:05 +0100

    Add files via upload

---
+++ Xleal.js
@@ -0,0 +1,14 @@
+
+
+loopy() 
+ function loopy() { 
+ var sWord ="mrwanxX" 
+ while (sWord != "Basurita_xit") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 XLEAL ");
+
+ } 
